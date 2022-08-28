038e023953abbfe47b37cc64b9aef6063e181559
QUBGames <johnivanhovich@gmail.com>
2022-08-28 15:39:45 +0300

    added main menu

--- app/src/main/java/com/foodmanager/MainActivity.java
+++ app/src/main/java/com/foodmanager/MainActivity.java
@@ -3,9 +3,9 @@ package com.foodmanager;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.view.Window;
 
 public class MainActivity extends AppCompatActivity {
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);

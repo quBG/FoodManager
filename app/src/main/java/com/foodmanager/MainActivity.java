b9b9196b2408d571ce2fe2d2c2f971cab9af78ff
QUBGames <johnivanhovich@gmail.com>
2022-09-03 21:07:28 +0300

    added pages to navigate

--- app/src/main/java/com/foodmanager/MainActivity.java
+++ app/src/main/java/com/foodmanager/MainActivity.java
@@ -1,15 +1,47 @@
 package com.foodmanager;
 
 import androidx.appcompat.app.AppCompatActivity;
-import androidx.appcompat.app.AppCompatDelegate;
 
+import android.content.Intent;
 import android.os.Bundle;
-import android.view.Window;
+import android.view.View;
+
+import com.foodmanager.pages.*;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+    }
+
+    public void toMyFridge(View view) {
+        Intent intent = new Intent(this, my_fridge.class);
+        startActivity(intent);
+    }
+
+    public void toCategories(View view) {
+        Intent intent = new Intent(this, categories.class);
+        startActivity(intent);
+    }
+
+    public void toSearch(View view) {
+        Intent intent = new Intent(this, search.class);
+        startActivity(intent);
+    }
+
+    public void toRandomRecipe(View view) {
+        Intent intent = new Intent(this, random_recipe.class);
+        startActivity(intent);
+    }
+
+    public void toStory(View view) {
+        Intent intent = new Intent(this, story.class);
+        startActivity(intent);
+    }
+
+    public void toStatistics(View view) {
+        Intent intent = new Intent(this, statistics.class);
+        startActivity(intent);
     }
 }

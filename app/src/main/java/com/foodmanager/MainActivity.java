c201799b1dce6b44447c6ef9115c77ef135588ca
QUBGames <johnivanhovich@gmail.com>
2022-10-11 20:33:22 +0300

    work on details

--- app/src/main/java/com/foodmanager/MainActivity.java
+++ app/src/main/java/com/foodmanager/MainActivity.java
@@ -6,14 +6,16 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.foodmanager.api.Connector;
 import com.foodmanager.pages.*;
+
+import java.util.Objects;
 
 public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Objects.requireNonNull(getSupportActionBar()).hide();
     }
 
     public void toMyFridge(View view) {
--- app/src/main/java/com/foodmanager/pages/Categories.java
+++ app/src/main/java/com/foodmanager/pages/Categories.java
@@ -2,11 +2,9 @@ package com.foodmanager.pages;
 
 import androidx.appcompat.app.AppCompatActivity;
 
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 
-import com.foodmanager.MainActivity;
 import com.foodmanager.R;
 
 public class Categories extends AppCompatActivity implements Temp {

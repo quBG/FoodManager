e3db78040b39bd5ced937e540049ca15c1963af0
QUBGames <johnivanhovich@gmail.com>
2022-10-14 20:20:12 +0300

    style rework

--- app/src/main/java/com/foodmanager/pages/MyFridge.java
+++ app/src/main/java/com/foodmanager/pages/MyFridge.java
@@ -4,6 +4,8 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
 
 import com.foodmanager.R;
 
@@ -16,6 +18,16 @@ public class MyFridge extends AppCompatActivity {
         setContentView(R.layout.activity_my_fridge);
         Objects.requireNonNull(getSupportActionBar()).setTitle(R.string.myFridgeFrameName);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        final Button buttonSelect = findViewById(R.id.selectProducts);
+        buttonSelect.setOnClickListener(view -> {
+            //SELECT PRODUCTS
+        });
+
+        final View buttonAdd = findViewById(R.id.addProduct);
+        buttonAdd.setOnClickListener(view -> {
+            //ADDING PRODUCT
+        });
     }
 
     @Override
